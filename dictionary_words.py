edd1532dc611e37fc6741b8ecde148c71641e2da
Duncan <buzzbuzz.macdonald@gmail.com>
2017-10-30 11:56:59 -0700

    added random dictionary words
    No stretch challenges yet

---
+++ dictionary_words.py
@@ -0,0 +1,18 @@
+import random
+import sys
+
+def dictionary_words(numOfWords):
+    words = []
+    f = open('/usr/share/dict/words', 'r', encoding='utf-8')
+
+    for line in f:
+        words.append(line)
+
+    for _ in range(0, numOfWords):
+        randy = random.randint(0, len(words))
+        random_word = words[randy]
+        # takes the newline off the end of each word
+        print(random_word[0:-1])
+
+
+dictionary_words(int(sys.argv[1]))
