135b2f44e9eacd11541a799192edb8c124b76094
Duncan <buzzbuzz.macdonald@gmail.com>
2017-11-07 10:07:33 -0800

    nice

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask
+from flask_restful import Resource, Api
 from histogram import CorpusHistogram
 
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/')
-def nice():
-    neat = CorpusHistogram('surgery.txt', False)
-    sentence = ''
-    for _ in range(0, 25):
-        word = neat.get_random_weighted_word() + ' '
-        sentence += word
-    return sentence
+class Tweet(Resource):
+    def get(self, num_of_words=20):
+        histogram = CorpusHistogram('surgery.txt', False)
+        sentence = ''
 
+        for _ in range(0, num_of_words):
+            word = histogram.get_random_weighted_word() + ' '
+            sentence += word
+
+        return sentence
+
+
+api.add_resource(Tweet, '/<int:num_of_words>')
 
 if __name__ == '__main__':
     app.run(debug=True)
