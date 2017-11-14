e3d262e789934cf839dc93ebd156659b1443795c
Duncan <buzzbuzz.macdonald@gmail.com>
2017-11-14 13:14:55 -0800

    nice

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-from histogram import CorpusHistogram
+from markov_chain import MarkovChain
 
 
 app = Flask(__name__)
 api = Api(app)
+
+
+def split_text_file(corpus):
+    f = open(corpus, 'r', encoding='utf-8')
+    words = f.read().lower().replace('\n', ' ')
+    words = re.sub('[^a-z]+', ' ', words)
+    words = words.split(' ')
+    return words
 
 
 class Tweet(Resource):
@@ -12,14 +20,13 @@ class Tweet(Resource):
         num_of_words = 20
         if request.args['num'] is not None:
             num_of_words = int(request.args['num'])
-        histogram = CorpusHistogram('surgery.txt', False)
-        sentence = ''
+        histogram = MarkovChain(split_text_file('surgery.txt'))
+        sentence = []
 
         for _ in range(0, num_of_words):
-            word = histogram.get_random_weighted_word() + ' '
-            sentence += word
+            sentence.append(histogram.get_random_weighted_word())
 
-        return sentence
+        return ' '.join(sentence)
 
 
 api.add_resource(Tweet, '/')
