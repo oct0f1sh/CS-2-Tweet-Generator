389bacbcbe3a754cc1b6af36bd22bafcd72c2ff0
Duncan <buzzbuzz.macdonald@gmail.com>
2017-11-14 11:54:29 -0800

    git is weird

--- listogram.py
+++ listogram.py
@@ -1,50 +1,3 @@
-# import time
-# import re
-# import random
-#
-#
-# class Dictogram(dict):
-#     def __init__(self, corpus):
-#         self.corpus = corpus
-#         self.elapsed_time = None
-#         # dict.__init__(self)
-#         self = self.get_histogram(corpus)
-#
-#     def get_histogram(self, source_text):
-#         '''Reads the source text and generates/returns the completed histogram'''
-#         start_time = time.time()
-#         f = open(source_text, 'r', encoding='utf-8')
-#         words = f.read().lower().replace('\n', ' ')
-#         words = re.sub('[^a-z]+', ' ', words)
-#         words = words.split(' ')
-#
-#         unsorted_dictogram = Dictogram.check_word_in_histogram(words)
-#
-#         elapsed_time = time.time() - start_time
-#
-#         for sorted_item in sorted(unsorted_dictogram, key=lambda item: item[1]):
-#             item_text = '\"{}\" appears {} times'.format(sorted_item[0], sorted_item[1])
-#             self[sorted_item[0]] = sorted_item[1]
-#
-#         self.elapsed_time = '\nElapsed time: {} seconds'.format(elapsed_time)
-#
-#         f.close()
-#
-#         return self
-#
-#     @staticmethod
-#     def check_word_in_histogram(words):
-#         '''Checks if word is in histogram
-#         if word is in histogram then it will increase the count by one
-#         if word is not in histogram then it will add it and initialize count to 0
-#         i don't really understand why this works but tony showed it to me'''
-#         info_histogram = {}
-#         tony_is_weird = info_histogram.get
-#         for word in words:
-#             info_histogram[word] = tony_is_weird(word, 0) + 1
-#         return info_histogram
-
-
 from __future__ import division, print_function  # Python 2 and 3 compatibility
 
 
@@ -65,19 +18,46 @@ class Listogram(list):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
+        self.tokens += count
+
+        if len(self) is 0:
+            self.append([word, count])
+            self.types += 1
+            return
+        else:
+            for word_list_item in self:
+                if word_list_item[0] == word:
+                    word_list_item[1] += count
+                    return
+        self.append([word, count])
+        self.types += 1
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        for word_list_item in self:
+            if word_list_item[0] == word:
+                frequency = word_list_item[1]
+                return frequency
+        return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
         # TODO: Check if word is in this histogram
+        for word_list_item in self:
+            if word == word_list_item[0]:
+                return True
+        return False
 
     def _index(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
         # TODO: Implement linear search to find index of entry with target word
+        index = 1
+        for word_list_item in self:
+            if target == word_list_item[0]:
+                return index
+        return None
 
 
 def print_histogram(word_list):
